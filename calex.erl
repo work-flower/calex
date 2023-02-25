97fec0be402702a127b048370a4f5f507cf7d747
Aylin Beynur Ahmed <ahmetaylinozturk@gmail.com>
2023-02-25 14:34:07 +0000

    new functions added

--- calex.erl
+++ calex.erl
@@ -14,14 +14,24 @@
 	 next_year/1,
 	 last_year/0,
 	 last_year/1,
-	 end_of_the_day/0,
-	 end_of_the_day/1,
-	 start_of_the_day/0,
-	 start_of_the_day/1,
 	 start_of_the_week/0,
 	 start_of_the_week/1,
+	 start_of_the_day/0,
+	 start_of_the_day/1,
+	 start_of_the_hour/0,
+	 start_of_the_hour/1,
+	 start_of_the_minute/0,
+	 start_of_the_minute/1,
+	 middle_of_the_day/0,
+	 middle_of_the_day/1,
 	 end_of_the_week/0,
 	 end_of_the_week/1,
+	 end_of_the_day/0,
+	 end_of_the_day/1,
+	 end_of_the_hour/0,
+	 end_of_the_hour/1,
+	 end_of_the_minute/0,
+	 end_of_the_minute/1,
 	 dayname_of_the_week/0, 
 	 dayname_of_the_week/1
 	]).
@@ -50,31 +60,66 @@ last_year(DateTime) ->
 last_year() ->
 	last_year(calendar:local_time()).
 
+end_of_the_week(DateTime) ->
+	{Date, _} = DateTime,
+	DaysToEndOfTheWeek = 7-calendar:day_of_the_week(Date),
+	add(day, DaysToEndOfTheWeek, {Date, {23,59,59}}).
+end_of_the_week() ->
+	end_of_the_week(calendar:local_time()).
+
 end_of_the_day(DateTime) ->
 	{Date, _} = DateTime,
 	{Date, {23, 59, 59}}.
 end_of_the_day() ->
 	end_of_the_day(calendar:local_time()).
 
-start_of_the_day(DateTime)->
+end_of_the_hour(DateTime) ->
+	{Date, Time} = DateTime,
+	{Hour, _, _} = Time,
+	{Date, {Hour, 59, 59}}.
+end_of_the_hour() ->
+	end_of_the_hour(calendar:local_time()).
+
+end_of_the_minute(DateTime) ->
+	{Date, Time} = DateTime,
+	{Hour, Minute, _} = Time,
+	{Date, {Hour, Minute, 59}}.
+end_of_the_minute() ->
+	end_of_the_minute(calendar:local_time()).
+
+middle_of_the_day(DateTime) ->
+	{Date, _} = DateTime,
+	{Date, {12, 0, 0}}.
+middle_of_the_day() ->
+	middle_of_the_day(calendar:local_time()).
+
+start_of_the_week(DateTime) ->
+	{Date, _} = DateTime,
+	DaysToStartOfTheWeek = -day_of_the_week(Date),
+	add(day, DaysToStartOfTheWeek, {Date, {0,0,0}}).
+start_of_the_week() ->
+	start_of_the_week(calendar:local_time()).
+
+start_of_the_day(DateTime) ->
 	{Date, _} = DateTime,
 	{Date, {0,0,0}}.
 start_of_the_day() ->
 	start_of_the_day(calendar:local_time()).
 
-start_of_the_week(DateTime) ->
-	{Date, _} = DateTime,
-	DaysToStartOfTheWeek = -day_of_the_week(Date),
-	add(day, DaysToStartOfTheWeek, DateTime).
-start_of_the_week() ->
-	start_of_the_week(calendar:local_time()).
+start_of_the_hour(DateTime) ->
+	{Date, Time} = DateTime,
+	{Hour, _, _} = Time,
+	{Date, {Hour, 0, 0}}.
+start_of_the_hour() ->
+	start_of_the_hour(calendar:local_time()).
 
-end_of_the_week(DateTime) ->
-	{Date, _} = DateTime,
-	DaysToEndOfTheWeek = 7-calendar:day_of_the_week(Date),
-	add(day, DaysToEndOfTheWeek, DateTime).
-end_of_the_week() ->
-	end_of_the_week(calendar:local_time()).
+start_of_the_minute(DateTime) ->
+	{Date, Time} = DateTime,
+	{Hour, Minute, _} = Time,
+	{Date, {Hour, Minute, 0}}.
+start_of_the_minute() ->
+	start_of_the_minute(clendar:local_time()).
+
 
 add(year, Years, DateTime) ->
 	{{Year, Month, Day}, Time} = DateTime,
--- calex_TEST.erl
+++ calex_TEST.erl
@@ -10,15 +10,25 @@
          next_year/1,
          last_year/0,
          last_year/1,
-         end_of_the_day/0,
-         end_of_the_day/1,
-         start_of_the_day/0,
-         start_of_the_day/1,
          start_of_the_week/0,
          start_of_the_week/1,
+         start_of_the_day/0,
+         start_of_the_day/1,
+         start_of_the_hour/0,
+         start_of_the_hour/1,
+         start_of_the_minute/0,
+         start_of_the_minute/1,
+         middle_of_the_day/0,
+         middle_of_the_day/1,
          end_of_the_week/0,
          end_of_the_week/1,
-         dayname_of_the_week/0,
+         end_of_the_day/0,
+         end_of_the_day/1,
+         end_of_the_hour/0,
+         end_of_the_hour/1,
+         end_of_the_minute/0,
+         end_of_the_minute/1,
+	 dayname_of_the_week/0,
          dayname_of_the_week/1
        ]
        ).
@@ -29,20 +39,27 @@ test() ->
 	test_individual(DateTime).
 
 test_individual(DateTime) ->
+	io:format("TEST Against Date Time ~p~n", [DateTime]),
 	{Date, Time} = DateTime,
+	{Hour, Minute, Second} = Time,
 	eval(tomorrow(DateTime), {2023,02,24}, tomorrow),
         eval(yesterday(DateTime), {2023, 02, 22}, yesterday),
 	eval(next_year(DateTime), {{2024, 02, 23}, Time}, next_year),
 	eval(last_year(DateTime), {{2022,02,23},Time}, last_year),
-	eval(end_of_the_day(DateTime), {Date, {23,59,59}}, end_of_the_day),
-	eval(start_of_the_day(DateTime), {Date, {0,0,0}}, start_of_the_day),
 	eval(end_of_the_week(DateTime), {{2023, 02, 26},{23,59,59}}, end_of_the_week),
-	eval(start_of_the_week(DateTime), {{2023, 02, 20}, {0,0,0}}, start_of_the_week).
+	eval(end_of_the_day(DateTime), {Date, {23,59,59}}, end_of_the_day),
+	eval(end_of_the_hour(DateTime), {Date, {Hour, 59, 59}},end_of_the_hour),
+	eval(end_of_the_minute(DateTime), {Date, {Hour, Minute, 59}}, end_of_the_minute),
+	eval(start_of_the_week(DateTime), {{2023, 02, 19}, {0,0,0}}, start_of_the_week),
+	eval(start_of_the_day(DateTime), {Date, {0,0,0}}, start_of_the_day),
+	eval(start_of_the_hour(DateTime), {Date, {Hour,0, 0}}, start_of_the_hour),
+	eval(start_of_the_minute(DateTime), {Date, {Hour, Minute,0}}, start_of_the_minute).
 
 eval(LeftPredicate, RightPredicate, FunctionName) ->
-	eval(LeftPredicate =:= RightPredicate, string:join([FunctionName, ":", io:format("~p", [LeftPredicate]), "<->", io:format("~p", [RightPredicate])], " ")).
-eval(true, Description) ->
-	io:format("PASS: ~p~n", [Description]);
-eval(false, Description) ->
-	io:format("!>> FAIL: ~p~n", [Description]).
+	eval(LeftPredicate =:= RightPredicate),
+	io:format(": ~p(~n\t~p, ~n\t~p) ~n", [FunctionName, LeftPredicate, RightPredicate]).
+eval(true) ->
+	io:write(pass);
+eval(false) ->
+	io:write(fail).
 

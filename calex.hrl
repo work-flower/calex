dc1901019d1fa337acbcb87a92909e970e77884d
work-flower <ahmetaylinozturk@gmail.com>
2023-03-13 13:01:52 +0000

    basic record definitions added

---
+++ calex.hrl
@@ -0,0 +1,40 @@
+-define(
+	MONTH_NAMES, 
+	[
+	 {
+		en,
+		[
+			{1, 'January', "January", 'Jan', "Jan"}, 
+			{2, 'February', "February", 'Feb', "Feb"}, 
+			{3, 'March', "March", 'Mar', "Mar"}, 
+			{4, 'April', "April", 'Apr', "Apr"}, 
+			{5, 'May', "May", 'May', "May"}, 
+			{6, 'June', "June", 'Jun', "June"}, 
+			{7, 'July', "July", 'Jul', "Jul"}, 
+			{8, 'August', "August", 'Aug', "Aug"}, 
+			{9, 'September', "September", 'Sep', "Sep"}, 
+			{10, 'October', "October", 'Oct', "Oct"}, 
+			{11, 'November', "November", 'Nov', "Nov"}, 
+			{12, 'December', "December", 'Dec', "Dec"} 
+		]
+	 }
+	]
+).
+
+-define(
+	DAY_NAMES, 
+	[
+	  {
+	    en,
+		[
+			{1, 2, 'Monday', "Monday", 'Mon', "Mon"}, 
+			{2, 3, 'Tuesday', "Tursday", 'Tue', "Tue"}, 
+			{3, 4, 'Wednesday', "Wednesday", 'Wed', "Wed"}, 
+			{4, 5, 'Thursday', "Thursday", 'Thu', "Thu"}, 
+			{5, 6, 'Friday', "Friday", 'Fri', "Fri"}, 
+			{6, 7, 'Saturday', "Saturday", 'Sat', "Sat"}, 
+			{7, 1, 'Sunday', "Sunday", 'Sun', "Sun"}
+		]
+	  }
+	]
+).                                                                           
